b0f24fd2f1b3e709bd9c434579c6b5bffdc98ff5
GuilhermeLouback <guilherme_lou@hotmail.com>
2023-05-08 21:59:53 -0300

    Criando View

--- public/index.php
+++ public/index.php
@@ -1,17 +1,3 @@
 <?php
 require_once(dirname(__FILE__,2). '/src/config/config.php');
-require_once(dirname(__FILE__,2). '/src/models/user.php');
-
-
-$user = new User(['name'=>'Guilherme', 'email' => 'guilherme_lou@hotmail.com']);
-print_r($user);
-echo '<br><br>';
-$user -> email = 'guilherme_alterar@email.com';
-print_r($user -> email);
-
-
-
-
-
-
-echo 'fim!';
+require_once(dirname(__FILE__,2). '/src/views/login.php');
